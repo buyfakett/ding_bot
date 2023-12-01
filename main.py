10e0cfd0235364f63a1dc62d23d896718d1a9efb
buyfakett <buyfakett@vip.qq.com>
2023-12-01 22:30:57 +0800

    测试增加数据库-3

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from dingtalk_stream import AckMessage
 import dingtalk_stream
 from util.yaml_util import read_yaml
 import requests
-from sqlalchemy import create_engine
-from develop.project import develop_project
-from develop.nginx import develop_nginx
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+# 定义模型类
+Base = declarative_base()
 
 
 def setup_logger():
@@ -19,6 +21,58 @@ def setup_logger():
     logger.addHandler(handler)
     logger.setLevel(logging.INFO)
     return logger
+
+
+def develop_nginx(expression, param_flag):
+    nginxs = read_yaml('nginx', 'config')
+    nginx_url = read_yaml('nginx-url', 'config')
+    response = ''
+    flag = 0
+    if param_flag:
+        response += '更新nginx配置：'
+        for nginx in nginxs:
+            response += '\n' + '更新 ' + nginx['server-name'] + ' 请回复：@jenkins服务一键上线 2 ' + str(nginx['id'])
+    else:
+        for nginx in nginxs:
+            if expression == str(nginx['id']):
+                try:
+                    data = {
+                        'ServerNameList': nginx['server-name']
+                    }
+                    requests.get(str(nginx_url), json=data, verify=False)
+                except:
+                    response = '调用上线接口成功'
+                else:
+                    response = '调用更新nginx配置接口成功， ' + str(nginx['server-name']) + ' 正在更新！'
+            else:
+                flag += 1
+            if flag == nginx['id']:
+                response = '没有该服务器'
+    return response
+
+
+def develop_project(expression, param_flag):
+    projects = read_yaml('project', 'config')
+    response = ''
+    flag = 0
+    if param_flag:
+        response += '上线项目：'
+        for project in projects:
+            response += '\n' + '上线 ' + project['name'] + ' 请回复：@jenkins服务一键上线 1 ' + str(project['id'])
+    else:
+        for project in projects:
+            if expression == str(project['id']):
+                try:
+                    requests.get(str(project['url']), verify=False)
+                except:
+                    response = '调用上线接口成功'
+                else:
+                    response = '调用上线接口成功， ' + str(project['name']) + ' 正在上线！'
+            else:
+                flag += 1
+            if flag == project['id']:
+                response = '没有该项目'
+    return response
 
 
 class CalcBotHandler(dingtalk_stream.ChatbotHandler):
@@ -71,6 +125,19 @@ def main():
     client.start_forever()
 
 
+class Project(Base):
+    __tablename__ = 'develop_project'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    url = Column(String(255), nullable=False)
+
+
+class Nginx(Base):
+    __tablename__ = 'develop_nginx'
+    id = Column(Integer, primary_key=True, index=True)
+    server_name = Column(String(255), nullable=False)
+
+
 if __name__ == '__main__':
     db_user = read_yaml('user', 'db')
     db_password = read_yaml('password', 'db')
@@ -79,4 +146,9 @@ if __name__ == '__main__':
     db_database = read_yaml('database', 'db')
     DATABASE_URL = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_database}"
     engine = create_engine(DATABASE_URL)
+    # 创建表
+    Base.metadata.create_all(bind=engine)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    session = SessionLocal()
+    session.close()
     main()
